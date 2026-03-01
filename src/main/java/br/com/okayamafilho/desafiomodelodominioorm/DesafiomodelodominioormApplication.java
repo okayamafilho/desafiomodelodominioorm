1b1e4326e1bd72b572924265a025befab42de4a6
Toshiaki Okayama Filho <okayamafilho@gmail.com>
2026-03-01 14:43:50 -0300

    primeiro commit

---
+++ src/main/java/br/com/okayamafilho/desafiomodelodominioorm/DesafiomodelodominioormApplication.java
@@ -0,0 +1,13 @@
+package br.com.okayamafilho.desafiomodelodominioorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiomodelodominioormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiomodelodominioormApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/okayamafilho/desafiomodelodominioorm/entities/Atividade.java
@@ -0,0 +1,13 @@
+package br.com.okayamafilho.desafiomodelodominioorm.entities;
+
+public class Atividade {
+
+    private Integer id;
+    private String nome; 
+    private String descricao;
+    private Double preco;
+
+    
+
+
+}
