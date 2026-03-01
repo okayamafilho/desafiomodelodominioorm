6fafdc227f9b576a06ca3996db211a056eee1260
Toshiaki Okayama Filho <okayamafilho@gmail.com>
2026-03-01 20:20:41 -0300

    alteracao da classe Participante colocando relacionamento de muitos para muitos com a classe Atividade

--- src/main/java/br/com/okayamafilho/desafiomodelodominioorm/entities/Participante.java
+++ src/main/java/br/com/okayamafilho/desafiomodelodominioorm/entities/Participante.java
@@ -1,11 +1,35 @@
 package br.com.okayamafilho.desafiomodelodominioorm.entities;
 
+import java.util.HashSet;
+import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_participante")
 public class Participante {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
+
+    @ManyToMany
+    @JoinTable(
+        name = "tb_participante_atividade",
+        joinColumns = @JoinColumn(name = "participante_id"),
+        inverseJoinColumns = @JoinColumn(name = "atividade_id")
+    )
+
+    private Set<Atividade> atividades = new HashSet<>();
 
     public Participante() {
 
@@ -20,20 +44,24 @@ public class Participante {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
-
 }
